033036730c645502d2556480fe9939865cee0ff8
Bin0Ary <120618010+Bin0Ary@users.noreply.github.com>
2025-01-09 03:35:43 +0330

    Merge Header.h

--- ProjectB/Header.h
+++ ProjectB/Header.h
@@ -2,9 +2,9 @@
 #include <iostream>
 #include <vector>
 #include <string>
-
+#include <deque>
 // Prompts the user for number inputs
-void NumberPrompt(std::string &a) {
+void NumberPrompt(std::string& a) {
 	std::cout << "Enter both numbers with one space between them: ";
 	std::getline(std::cin, a);
 }
@@ -12,7 +12,7 @@ void NumberPrompt(std::string &a) {
 by shifting all characters by one to the left everytime
 a null character is read, it will stop after reading
 the first non null character*/
-void CleanString(std::string &s) {
+void CleanString(std::string& s) {
 	for (size_t i{ 0 }; i < s.size(); i) {
 		if (s[i] == ' ') {
 			for (size_t j{ i + 1 }; j <= s.size(); j++) {
@@ -24,24 +24,90 @@ void CleanString(std::string &s) {
 		}
 	}
 }
-//Fills a 2d vector with the values inside the string
-//by creating a temporary vector and using pushback to
-//fill the temp vector and then pushes the temporary
-//vector into the input vector creating a row
-void FillVector(std::vector <std::vector<int>> &v, const std::string &s) {
-	size_t pos{0};
-	for (size_t i{ 0 }; i < 2; i++) {
-		std::vector <int> temp;
-		for (size_t j{pos}; j < s.size(); j++) {
-			if (s[j] == ' ') {
-				pos = (j + 1);
-				break;
-			}
-			else{temp.push_back(s[j] - 48); }
+/*Fills two deques with numbers inside the strings*/
+void FillDeque(std::deque <int>& numInt1, std::deque <int>& numInt2, const std::string& numString) {
+	size_t pos{ 0 };
+	for (size_t i{ 0 }; i < numString.size(); ++i) {
+		if (numString[i] == ' ') { pos = i + 1; break; }
+		else {
+			numInt1.push_back(numString[i] - 48);
 		}
-		v.push_back(temp);
 	}
-	
+	for (size_t i{ pos }; i < numString.size(); ++i) {
+		numInt2.push_back(numString[i] - 48);
+	}
+}
+/* Multiplication algorithm for the two deques*/
+void Mult(const std::deque <int>& numInt1, const std::deque <int>& numInt2, std::deque <std::deque<int>>& multInt) {
+	size_t carry{ 0 };
+	size_t mult{ 0 };
+	size_t carryCheck{ 0 };
+	// l variable is used to keep track of the number of zeroes to add 
+	size_t l = numInt1.size() - 1;
+	// i variable is used to keep track of the row in numInt1 deque
+	for (size_t i = numInt1.size(); i-- > 0;) {
+		std::deque <int> temp;
+		carry = 0;
+		// j variable is used to keep track of the row
+		for (size_t j = numInt2.size(); j-- > 0;) {
+			mult = ((numInt1[i] * numInt2[j]) + carry);
+			temp.push_front(mult % 10);
+			if (j == 0) {
+				if (mult > 9) {
+					temp.push_front(mult / 10);
+					carryCheck = 1;
+				}
+			}
+			carry = 0;
+			if (mult > 9) {
+				carry = mult / 10;
+			}
+		}
+		if (l > 0 && carryCheck != 1) {
+			for (size_t k = l; k-- > 0;) {
+				temp.push_front(0);
+			}
+		}
+		else if (carryCheck == 1 && l > 0) {
+            for (size_t k = l - 1; k > 0; --k) {
+            temp.push_front(0);
+            }
+		}
+		for (size_t k = 0; k < numInt1.size() - 1 - i; ++k) {
+			temp.push_back(0);
+		}
+		carryCheck = 0;
+		l--;
+		multInt.push_back(temp);
+	}
 }
 
-// TODO: Add the multiplication step
+void Add(std::deque <int>& resultInt, const std::deque <std::deque<int>>& multInt, size_t& l) {
+	unsigned int sum{ 0 };
+	unsigned int carry{ 0 };
+	for (size_t i = multInt[0].size(); i-- > 0;) {
+		sum = 0;
+		for (size_t j = multInt.size(); j-- > 0;) {
+			sum += multInt[j][i];
+		}
+		if (carry > 0) {
+			sum += carry;
+			carry = 0;
+		}
+		if (sum > 9) {
+			carry = sum / 10;
+		}
+		if(i > 0){
+		resultInt.push_front(sum % 10);
+		}
+		else {
+			resultInt.push_front(sum);
+		}
+	}
+}
+
+void DisplayDeque(const std::deque <int>& resultInt) {
+	for (auto val : resultInt) {
+		std::cout << val;
+	}
+}
