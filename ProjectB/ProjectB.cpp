79c423a7cc68aa1401ccfb09b9a83a8a09fafb1a
Bin0Ary <120618010+Bin0Ary@users.noreply.github.com>
2025-01-09 03:36:11 +0330

    Merge ProjectB.cpp

--- ProjectB/ProjectB.cpp
+++ ProjectB/ProjectB.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
+#include <deque>
 #include <string>
-#include <vector>
-#include "Header.h"
+#include "header.h"
 int main() {
-	std::string numString{};
+	std::string numString;
+	std::deque <int> numInt1;
+	std::deque <int> numInt2;
+	std::deque <std::deque<int>> multInt;
+	std::deque <int> resultInt;
 	NumberPrompt(numString);
-	std::vector <std::vector <int>> numInt;
 	CleanString(numString);
-	FillVector(numInt, numString);
+	FillDeque(numInt1, numInt2, numString);
+	Mult(numInt1, numInt2, multInt);
+	size_t l = numInt1.size() - 1;
+	Add(resultInt, multInt, l);
+	DisplayDeque(resultInt);
 }
-
