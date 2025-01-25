bf540755d88ed9c89d85053e2efa2913c35fc742
Bin0Ary <120618010+Bin0Ary@users.noreply.github.com>
2025-01-25 12:56:41 +0330

    Update Header.h

--- ProjectB/Header.h
+++ ProjectB/Header.h
@@ -2,94 +2,103 @@
 #include <iostream>
 #include <string>
 #include <deque>
+#include <algorithm>
+
 // Prompts the user for number inputs
 void NumberPrompt(std::string& a) {
-	std::cout << "Enter both numbers with one space between them: ";
-	std::getline(std::cin, a);
+    std::cout << "Enter both numbers with one space between them: ";
+    std::getline(std::cin, a);
 }
-/*Deletes all null characters at the begining of a string
-by shifting all characters by one to the left everytime
-a null character is read, it will stop after reading
-the first non null character*/
+
+// Deletes all null characters at the beginning of a string
 void CleanString(std::string& s) {
-	for (size_t i{ 0 }; i < s.size(); i) {
-		if (s[i] == ' ') {
-			for (size_t j{ i + 1 }; j <= s.size(); j++) {
-				s[j - 1] = s[j];
-			}
-		}
-		else if (s[i] != ' ') {
-			return;
-		}
-	}
-}
-/*Fills two deques with numbers inside the strings by spilitting both*/
-void FillDeque(std::deque <int>& numInt1, std::deque <int>& numInt2, const std::string& numString) {
-	size_t pos{ 0 };
-	for (size_t i{ 0 }; i < numString.size(); ++i) {
-		if (numString[i] == ' ') { pos = i + 1; break; }
-		else {
-			numInt1.push_back(numString[i] - 48);
-		}
-	}
-	for (size_t i{ pos }; i < numString.size(); ++i) {
-		numInt2.push_back(numString[i] - 48);
-	}
-}
-/* Multiplication algorithm for the two deques*/
-void Mult(const std::deque <int>& numInt1, const std::deque <int>& numInt2, std::deque <std::deque<int>>& multInt) {
-	size_t carry{ 0 };
-	size_t mult{ 0 };
-	size_t l = 0; 
-	for (size_t i = numInt1.size(); i-- > 0;) {
-		std::deque<int> temp;
-		carry = 0;
-		for (size_t j = numInt2.size(); j-- > 0;) {
-			mult = (numInt1[i] * numInt2[j]) + carry;
-			temp.push_front(mult % 10);
-			carry = mult / 10;
-		}
-		if (carry > 0) {
-			temp.push_front(carry);
-		}
-		for (size_t k = 0; k < l; ++k) {
-			temp.push_back(0); 
-		}
-		multInt.push_back(temp);
-		l++; 
-	}
+    s.erase(0, s.find_first_not_of(' '));
 }
 
-//Add function, just adds the columns of the multInt deque
-void Add(std::deque <int>& resultInt, const std::deque <std::deque<int>>& multInt, size_t& l) {
-	unsigned int sum{ 0 };
-	unsigned int carry{ 0 };
-	size_t maxSize = 0;
-	for (const auto& row : multInt) {
-		if (row.size() > maxSize) {
-			maxSize = row.size();
-		}
-	}
-	for (size_t i = 0; i < maxSize; ++i) {
-		sum = carry;  
-		for (const auto& row : multInt) {
-			if (row.size() > i) {
-				sum += row[row.size() - 1 - i]; 
-			}
-		}
-		carry = sum / 10;
+// Fills two deques with numbers inside the strings by splitting both
+void FillDeque(std::deque<int>& numInt1, std::deque<int>& numInt2, const std::string& numString) {
+    size_t pos = numString.find(' ');
+    std::string num1 = numString.substr(0, pos);
+    std::string num2 = numString.substr(pos + 1);
 
-		resultInt.push_front(sum % 10);
-	}
+    bool isNegative1 = num1[0] == '-';
+    bool isNegative2 = num2[0] == '-';
 
-	while (carry > 0) {
-		resultInt.push_front(carry % 10);
-		carry /= 10;
-	}
+    if (isNegative1) num1 = num1.substr(1);
+    if (isNegative2) num2 = num2.substr(1);
+
+    for (char c : num1) {
+        numInt1.push_back(c - '0');
+    }
+
+    for (char c : num2) {
+        numInt2.push_back(c - '0');
+    }
+
+    if (isNegative1) numInt1[0] *= -1;
+    if (isNegative2) numInt2[0] *= -1;
 }
 
-void DisplayDeque(const std::deque <int>& resultInt) {
-	for (auto val : resultInt) {
-		std::cout << val;
-	}
+// Multiplication algorithm for the two deques
+void Mult(const std::deque<int>& numInt1, const std::deque<int>& numInt2, std::deque<std::deque<int>>& multInt) {
+    size_t carry = 0;
+    size_t mult = 0;
+    size_t l = 0;
+
+    for (size_t i = numInt1.size(); i-- > 0;) {
+        std::deque<int> temp;
+        carry = 0;
+        for (size_t j = numInt2.size(); j-- > 0;) {
+            mult = (numInt1[i] * numInt2[j]) + carry;
+            temp.push_front(mult % 10);
+            carry = mult / 10;
+        }
+        if (carry > 0) {
+            temp.push_front(carry);
+        }
+        for (size_t k = 0; k < l; ++k) {
+            temp.push_back(0);
+        }
+        multInt.push_back(temp);
+        l++;
+    }
+}
+
+// Add function, just adds the columns of the multInt deque
+void Add(std::deque<int>& resultInt, const std::deque<std::deque<int>>& multInt, size_t& l) {
+    unsigned int sum = 0;
+    unsigned int carry = 0;
+    size_t maxSize = 0;
+
+    for (const auto& row : multInt) {
+        if (row.size() > maxSize) {
+            maxSize = row.size();
+        }
+    }
+
+    for (size_t i = 0; i < maxSize; ++i) {
+        sum = carry;
+        for (const auto& row : multInt) {
+            if (row.size() > i) {
+                sum += row[row.size() - 1 - i];
+            }
+        }
+        carry = sum / 10;
+        resultInt.push_front(sum % 10);
+    }
+
+    while (carry > 0) {
+        resultInt.push_front(carry % 10);
+        carry /= 10;
+    }
+}
+
+void DisplayDeque(const std::deque<int>& resultInt, bool isResultNegative) {
+    if (isResultNegative) {
+        std::cout << "-";
+    }
+    for (auto val : resultInt) {
+        std::cout << std::abs(val);
+    }
+    std::cout << std::endl;
 }
