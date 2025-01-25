8c76d5f62a8cc349cbe4f97e8ef13d79f522934e
Bin0Ary <120618010+Bin0Ary@users.noreply.github.com>
2025-01-25 12:57:43 +0330

    Update ProjectB.cpp

--- ProjectB/ProjectB.cpp
+++ ProjectB/ProjectB.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
 #include <deque>
 #include <string>
-#include "header.h"
+#include "Header.h"
+
 int main() {
-	std::string numString;
-	std::deque <int> numInt1;
-	std::deque <int> numInt2;
-	std::deque <std::deque<int>> multInt;
-	std::deque <int> resultInt;
-	NumberPrompt(numString);
-	CleanString(numString);
-	FillDeque(numInt1, numInt2, numString);
-	Mult(numInt1, numInt2, multInt);
-	size_t l = numInt1.size() - 1;
-	Add(resultInt, multInt, l);
-	DisplayDeque(resultInt);
+    std::string numString;
+    std::deque<int> numInt1;
+    std::deque<int> numInt2;
+    std::deque<std::deque<int>> multInt;
+    std::deque<int> resultInt;
+
+    NumberPrompt(numString);
+    CleanString(numString);
+    FillDeque(numInt1, numInt2, numString);
+
+    bool isResultNegative = (numInt1[0] < 0) ^ (numInt2[0] < 0);
+
+    if (numInt1[0] < 0) numInt1[0] *= -1;
+    if (numInt2[0] < 0) numInt2[0] *= -1;
+
+    Mult(numInt1, numInt2, multInt);
+    size_t l = numInt1.size() - 1;
+    Add(resultInt, multInt, l);
+    DisplayDeque(resultInt, isResultNegative);
+
+    return 0;
 }
